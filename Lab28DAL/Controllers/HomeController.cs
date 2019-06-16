a14c97cbac2dda7c7ca7a3ea99b83715916f55ef
Steven <orionsjv@gmail.com>
2019-06-16 10:21:52 -0400

    First commit

---
+++ Lab28DAL/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Lab28DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab28DAL.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult CardList()
+        {
+            Deck d;
+
+            if(Session["deck"] == null)
+            {
+                d = CardAPIDAL.GetNewDeck();
+                Session["deck"] = d;
+            }
+            else
+            {
+                d = (Deck) Session["deck"];
+            }
+
+            Session["deck"] = CardAPIDAL.UpdateDeck(d);
+
+            List<Card> cards = CardAPIDAL.DrawCards(5, d);
+            return View(cards);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
